1b57b73379e2a0c72a1b01f1ad784d68aa2f3723
Viva <happybird00@naver.com>
2026-02-03 10:06:54 +0900

    init

--- src/main/java/org/example/chatting/common/interceptor/StompAuthInterceptor.java
+++ src/main/java/org/example/chatting/common/interceptor/StompAuthInterceptor.java
@@ -1,4 +1,46 @@
 package org.example.chatting.common.interceptor;
 
-public class StompAuthInterceptor {
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.example.chatting.common.entity.User;
+import org.example.chatting.common.utils.JwtUtil;
+import org.example.chatting.domain.repository.UserRepository;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StompAuthInterceptor implements ChannelInterceptor {
+
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+        // 🔥 STOMP CONNECT 시점
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+
+            String token = accessor.getFirstNativeHeader("Authorization")
+                .replace("Bearer ", "");
+
+            Long userId = jwtUtil.getUserId(token);
+
+            User user = userRepository.findById(userId)
+                .orElseThrow();
+
+            StompUserPrincipal principal = new StompUserPrincipal(user, user.getName());
+
+            // 이 WebSocket 연결의 주인 설정
+            accessor.setUser(principal);
+        }
+
+        return message;
+    }
 }
--- src/main/java/org/example/chatting/common/interceptor/StompUserPrincipal.java
+++ src/main/java/org/example/chatting/common/interceptor/StompUserPrincipal.java
@@ -1,4 +1,20 @@
 package org.example.chatting.common.interceptor;
 
-public class StompUserPrincipal {
+import java.security.Principal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.example.chatting.common.entity.User;
+
+@Getter
+@AllArgsConstructor
+public class StompUserPrincipal implements Principal {
+
+    private final User user;
+    private final String username;
+
+    @Override
+    public String getName() {
+        return username;
+    }
+
 }
